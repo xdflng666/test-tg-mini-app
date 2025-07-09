af2ecd4311b89969897cbf69db3fcd3fcf9ec1f9
Daniil Slyusarev <daniil.slyusarev@lamoda.ru>
2025-07-10 02:01:12 +0300

    some setup changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template/',
+  base: '/test-tg-mini-app',
   css: {
     preprocessorOptions: {
       scss: {
